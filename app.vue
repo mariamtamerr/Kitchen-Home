9b3eb24e7480893a1be79114529e8b0f35456046
mariamtamerr <memariam5@outlook.com>
2024-11-07 02:17:07 +0200

    plugins

--- app.vue
+++ app.vue
@@ -10,39 +10,18 @@
 } */
 
 
-@font-face {
-    font-family: 'PingAR+LT-Regular';
-    src: url('@/assets/fonts/Ping/PingAR+LT-Regular.otf') format('opentype');
-    font-weight: 400;
-    /* font-style: normal; */
-  }
-  
-  @font-face {
-    font-family: 'PingAR+LT-Bold';
-    src: url('@/assets/fonts/Ping/PingAR+LT-Bold.otf') format('opentype');
-    /* font-weight: 800; */
-    /* font-style: normal; */
-  }
-  
   @font-face {
     font-family: 'PingAR+LT-Black';
-    src: url('@/assets/fonts/Ping/PingAR+LT-Black.otf') format('opentype');
+    src: url('/assets/fonts/Ping/PingAR+LT-Black.otf') format('opentype');
     /* font-weight: 900;
     font-style: normal; */
   }
   
-  @font-face {
-    font-family: 'PingAR+LT-Medium';
-    src: url('@/assets/fonts/Ping/PingAR+LT-Medium.otf') format('opentype');
-    /* font-weight: 900; */
-    /* font-style: normal; */
-  }
-  
-  
+
   
   @font-face {
     font-family: 'PingAR+LT-Heavy';
-    src: url('@/assets/fonts/Ping/PingAR+LT-Heavy.otf') format('opentype');
+    src: url('/assets/fonts/Ping/PingAR+LT-Heavy.otf') format('opentype');
     /* font-weight: 900; */
     /* font-style: normal; */
   }
